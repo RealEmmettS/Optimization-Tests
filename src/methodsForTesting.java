d3dea73d81daba03998101c57872681373cf5369
Emmett S <30877743+RealEmmettS@users.noreply.github.com>
2021-01-29 20:58:27 -0600

    init

---
+++ src/methodsForTesting.java
@@ -0,0 +1,81 @@
+
+public class methodsForTesting {
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////
+
+	public static void testEvenOdd(int numberOfTests) {
+		
+		for (int i=0; i<numberOfTests; i++) {
+			int randInt = (int) (Math.random()*1000);
+			System.out.println(randInt);
+			System.out.println("BITWISE: "+(evenBitwise(randInt) == true ? "Even" : "Odd"));
+			System.out.println("NORMAL: "+(evenNormal(randInt) == true ? "Even" : "Odd"));
+			System.out.println("\n");
+		}
+	}
+	
+
+	
+	public static boolean evenBitwise(int n) {
+		return ((n & 1)!=1); //check if last binary digit is 1. If so, it's odd.
+	}
+	
+	public  static boolean evenNormal(int n) {
+		boolean x;
+		if (n%2 == 0) { //divides by 2 and check if there's any remainder. If there is, it's odd.
+			x=true;
+		}else {
+			x=false;
+		}
+		
+		return x;
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	
+	public static void testMultiplyBy2(int numberToMultiply) {		//Version 1 - Multiplies parameter by 2
+		
+		int x1 = numberToMultiply*2;
+		int x2 = numberToMultiply<<1;
+		
+		System.out.println("BITWISE: "+x2);
+		System.out.println("NORMAL: "+x1);
+		System.out.println("\n\n");
+		
+	}
+	
+	public static void testMultiplyBy2(boolean x, int loops ) {		//Version 2 - Multiplies [2nd parameter] numbers by 2
+		
+		if (x = true){
+			for (int i=0; i<loops; i++) {
+				int r = (int) (Math.random()*1000);
+				int x1 = r*2;
+				int x2 = r<<1;
+				
+				System.out.println("BITWISE: "+x2);
+				System.out.println("NORMAL: "+x1);
+				System.out.println("\n\n");
+			}
+		}
+		
+	}
+	
+	
+	
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ src/testClass.java
@@ -0,0 +1,14 @@
+
+public class testClass {
+
+	public static void main(String[] args) {
+		
+		//methodsForTesting.testEvenOdd(1000);			//Tests [parameter] random numbers for even or odd
+		//methodsForTesting.testMultiplyBy2(20); 		//Version 1 - Multiplies parameter by 2
+		//methodsForTesting.testMultiplyBy2(true, 3);	//Version 2 - Multiplies [2nd parameter] numbers by 2
+		
+
+	}
+	
+
+}
